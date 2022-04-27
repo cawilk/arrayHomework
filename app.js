24df171dc26b8475b77502feea1010db1d6ee38a
Carl <wilkinson.carl123@gmail.com>
2022-04-26 21:02:04 -0400

    function

---
+++ app.js
@@ -0,0 +1,24 @@
+function addiction(a, b) {
+  var sum = a + b;
+  console.log(sum);
+  return sum;
+}
+addiction(7, 15);
+
+function codeClass(a, b) {
+  var subtract = a - b;
+  console.log(subtract);
+}
+codeClass(8, 14);
+
+function avalon(a, b) {
+  var multiply = a * b;
+  console.log(multiply);
+}
+avalon(12, 14);
+
+function apple(a, b) {
+  var sum = a / b;
+  console.log(sum);
+}
+apple(8, 10);
